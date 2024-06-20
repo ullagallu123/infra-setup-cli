c15b6fb377f64426b2e51196686faf8a6e07c656
sivaram <siva.ram.konka@gmail.com>
2024-06-20 09:06:49 +0000

    adding backend store state file

--- 01.vpc/provider.tf
+++ 01.vpc/provider.tf
@@ -5,7 +5,15 @@ terraform {
       version = "5.53.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "expense-ugl-infra-terraform"
+    key            = "expense/vpc/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "aws-infra-locking"
+  }
 }
+
 
 provider "aws" {
   region  = "us-east-1"
